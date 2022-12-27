e70b50cb6e5303675db0b8388dc843ea82a00358
thahseen53 <thahseen53@gmail.com>
2022-12-27 13:11:30 +0530

    ngrok is added

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const ngrok = require('ngrok');
+const PORT = process.env.PORT || 3030;
 const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
 const io = require("socket.io")(server);
@@ -57,4 +59,12 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(process.env.PORT || 3030);
+server.listen(PORT);
+(async function(){
+    const url = await ngrok.connect({
+        proto:'http',
+        addr:PORT,
+        authtoken:'2ItcO1pxPHwKl81EJuLodWGgOVi_5sd5GCq3BQbR1a2jJuFr3'
+    });
+    console.log(url);
+})();
